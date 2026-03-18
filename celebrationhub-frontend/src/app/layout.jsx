3e673785f04f2650d7a5035bed8063127149b5af
donald <donalddaboiku@gmail.com>
2026-03-18 11:58:37 +0100

    page and layout updated

--- celebrationhub-frontend/src/app/layout.jsx
+++ celebrationhub-frontend/src/app/layout.jsx
@@ -2,14 +2,18 @@ import Script from 'next/script';
 import './globals.css';
 
 export const metadata = {
-  title: 'CelebrationHub',
-  description: 'Birthday and Anniversary Management System',
-}
+    title: 'CelebrationHub',
+    description: 'Birthday and Anniversary Management System',
+};
+
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
             <body>
-                <Script src="https://js.paystack.co/v1/inline.js" strategy="lazyOnload" />
+                <Script
+                    src="https://js.paystack.co/v1/inline.js"
+                    strategy="lazyOnload"
+                />
                 {children}
             </body>
         </html>
